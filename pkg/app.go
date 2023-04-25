4181bfb6f61913564dd9d3c2dcca0f7625187ae1
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2023-04-25 22:41:02 +0200

    more comments

--- pkg/app.go
+++ pkg/app.go
@@ -12,6 +12,8 @@ type Application struct {
 	search *Search
 }
 
+// NewApplication creates a new instance of Application.
+// It creates a new Search object as well.
 func NewApplication() *Application {
 	application := Application{}
 
@@ -22,12 +24,15 @@ func NewApplication() *Application {
 	return &application
 }
 
+// setUpRoutes handles routes.
+// It is a good place for potential middleware.
 func (a *Application) setUpRoutes() {
 	a.router = chi.NewRouter()
 
 	a.router.Mount("/api/v0.1/search", a.search.Routes())
 }
 
+// Start initiates ListenAndServe on port 9000 in localhost.
 func (a *Application) Start() error {
 	return http.ListenAndServe("localhost:9000", a.router)
 }
