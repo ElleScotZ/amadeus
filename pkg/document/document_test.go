6754a2cdf8bd03c96921bdb06a65c535645a2a68
Nikolett Galbicsek <nikolett.galbicsek@orfo.de>
2023-04-25 20:47:04 +0200

    test file WIP. test1.txt added

--- pkg/document/document_test.go
+++ pkg/document/document_test.go
@@ -1,1 +1,36 @@
 package document
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetAll(t *testing.T) {
+	textLocation := []byte("test1.txt")
+	searchWord := "bla"
+
+	router := Routes()
+
+	// GET request
+	url := fmt.Sprintf("/%v", searchWord)
+
+	bodyReader := bytes.NewReader(textLocation)
+
+	request, err := http.NewRequest("GET", url, bodyReader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// NewRecorder implements a ResponseWriter for testing
+	responseWriter := httptest.NewRecorder()
+
+	// GET response
+	router.ServeHTTP(responseWriter, request)
+
+	if status := responseWriter.Code; status != http.StatusOK {
+		t.Error(status)
+	}
+}
